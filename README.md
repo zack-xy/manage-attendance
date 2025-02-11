fe3f39a6374822c908e1b37c4b80ebdf20bacbf6
zack zheng <zack_zhengxiyun@163.com>
2025-02-11 14:26:21 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# manage-attendance
+### 考勤后管系统
+
+#### backend 后端
+
+docker启动mongodb
+当前目录下运行`docker compose up -d`
+
+记录：
+
+进入到mongodb shell
+`docker exec -it docker-mongodb mongosh`
+
+#### frontend 前端
---
+++ backend/model/checks.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose')
+
+let checkSchema = new mongoose.Schema({
+  state: { type: String, default: '待审批' },
+  applicantid: { type: String, required: true },
+  applicantname: { type: String, required: true },
+  approverid: { type: String, required: true },
+  approvername: { type: String, required: true },
+  reason: { type: String, required: true },
+  time: { type: Array, required: true },
+  note: { type: String, required: true }
+})
+
+let CheckModel = mongoose.model('check', checkSchema)
+
+module.exports = CheckModel
